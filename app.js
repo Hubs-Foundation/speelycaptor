d382e2cc31099a302c0f8b3fef2a31e709081110
tanfarming <tanfarming@hotmail.com>
2022-08-25 09:25:04 -0400

    word

--- app.js
+++ app.js
@@ -23,10 +23,12 @@ app.get('/init', function (req, res) {
 
 app.get('/convert', function (req, res) {
   console.log(req.query)
+  const tempFileName = require('tmp').tmpNameSync()
+  console.log("tempFileName: ",tempFileName)
   lambda.convert(
       {
         queryStringParameters: req.query,
-        tempFileName: require('tmp').tmpNameSync()
+        tempFileName: tempFileName,
       }, 
       null,
       async function (something, callback){
