c0156d5a841a82fec54041b816cb0c60cedc1e1b
tanfarming <tanfarming@hotmail.com>
2022-08-25 09:19:50 -0400

    word

--- app.js
+++ app.js
@@ -21,20 +21,20 @@ app.get('/init', function (req, res) {
     )
   });
 
-  app.get('/convert', function (req, res) {
-    console.log(req.query)
-    lambda.convert(
-        {
-          queryStringParameters: req.query,
-          tempFileName: require('tmp').tmpNameSync()
-        }, 
-        null,
-        async function (something, callback){
-            console.log("callback: ", callback)
-            res.status(callback.statusCode).header(callback.headers).send(callback.body)
-        }
-    )
-  });
+app.get('/convert', function (req, res) {
+  console.log(req.query)
+  lambda.convert(
+      {
+        queryStringParameters: req.query,
+        tempFileName: require('tmp').tmpNameSync()
+      }, 
+      null,
+      async function (something, callback){
+          console.log("callback: ", callback)
+          res.status(callback.statusCode).header(callback.headers).send(callback.body)
+      }
+  )
+});
 
   app.listen(5000, function () {
     console.log('listening on :5000');
